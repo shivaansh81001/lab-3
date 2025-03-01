8803fb90b83afe3f74f85e03fdc497fbcc3a7dba
shivaansh81001 <shivaans@ualberta.ca>
2025-02-28 22:00:12 -0700

    part 4

--- A3_student.py
+++ A3_student.py
@@ -438,7 +438,7 @@ def part4():
     from skimage.feature import ORB, match_descriptors
 
     #reference = https://scikit-image.org/docs/stable/auto_examples/features_detection/plot_orb.html#sphx-glr-auto-examples-features-detection-plot-orb-py
-    extractor = ORB(n_keypoints=500, fast_threshold=0.05)
+    extractor = ORB(n_keypoints=4000)
     
     extractor.detect_and_extract(image0)
     keypoints1 = extractor.keypoints
@@ -448,9 +448,9 @@ def part4():
     keypoints2 = extractor.keypoints
     descriptor_2 = extractor.descriptors
 
-    matches12 = match_descriptors(descriptor_1, descriptor_2, cross_check=True)
+    matches12 = match_descriptors(descriptor_1, descriptor_2, cross_check=True,max_distance=0.5)
 
-    print(matches12.shape)
+    #print(matches12.shape)
     # TODO: initialize Brute-Force matcher and exclude outliers. See match descriptor function.
     # ...
 
@@ -460,13 +460,13 @@ def part4():
     from skimage.measure import ransac
     from skimage.transform import ProjectiveTransform
 
-    src,dst = np.array(keypoints1[matches12[:, 0]]), np.array(keypoints2[matches12[:, 1]])
+    src,dst = np.array(keypoints2[matches12[:, 1]][:, ::-1]), np.array(keypoints1[matches12[:, 0]][:, ::-1])
 
     model = ProjectiveTransform()
     model.estimate(src,dst)
 
-    print(src)
-    model_robust, inliers = ransac((src,dst), ProjectiveTransform, min_samples=4, residual_threshold=4,max_trials=10000)
+    #print(src)
+    model_robust, inliers = ransac((src,dst), ProjectiveTransform, min_samples=4, residual_threshold=2,max_trials=1000)
 
     # ------------- Warping ----------------
     #Next, we produce the panorama itself. The first step is to find the shape of the output image by considering the extents of all warped images.
@@ -523,8 +523,8 @@ def part4():
         return np.dstack((rgb, alpha))
 
     # TODO: add alpha to the image0 and image1
-    image0 = add_alpha(image0)
-    image1 = add_alpha(image1)
+    image0 = add_alpha(image0_)
+    image1 = add_alpha(image1_)
 
     min_shape = (
     min(image0.shape[0], image1.shape[0]),
@@ -533,8 +533,10 @@ def part4():
     image0 = image0[:min_shape[0], :min_shape[1], :]
     image1 = image1[:min_shape[0], :min_shape[1], :]
 
+    image0[:, :, 3] = image0[:, :, 3]//2
+    image1[:, :, 3] = image1[:, :, 3]//2
     # TODO: merge the alpha added image (only change the next line)
-    merged = image1+image0
+    merged = np.maximum(image0,image1)
 
     alpha = merged[..., 3]
     merged /= np.maximum(alpha, 1)[..., np.newaxis]
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-min_shape[1], :]
-    image1 = image1[:min_shape[0], :min_shape[1], :]
+astype(np.uint8)
